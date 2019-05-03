c22c9806acd8b93034679c1ea588d90141f38852
Jon Wilson <jtwilson@ashevillenc.gov>
2019-05-03 09:51:33 -0400

    toAddresses as env var

--- sendEmails.js
+++ sendEmails.js
@@ -4,19 +4,12 @@ const ses_sendemail = require('./ses_sendemail');
 
 const compiledFunction = pug.compileFile(path.join(__dirname, '/email.pug'));
 
-// listOfVendors[0].a_vendor_id,
-// listOfVendors[0].a_vendor_name,
-// listOfVendors[0].a_vend_alpha_sort,
-// listOfVendors[0].v_dba,
-// listOfVendors[0].keyword
-
-function sendEmails(listOfVendors) { // console.log(listOfVendors) a_vendor_id, a_vendor_name, a_vend_alpha_sort, v_dba, keyword
-    let emailAddr = 'jtwilson@ashevillenc.gov';
+function sendEmails(listOfVendors) {
+    let emailAddrs = JSON.parse(process.env.EMAIL_RECIPIENT_JSON);
     let pugObj = {};
     pugObj.listOfVendors = listOfVendors;
     let htmlEmail = compiledFunction(pugObj);
-    // console.log(htmlEmail)
-    ses_sendemail(emailAddr,htmlEmail);
+    ses_sendemail(emailAddrs,htmlEmail);
 }
 
 module.exports = sendEmails;
--- ses_sendemail.js
+++ ses_sendemail.js
@@ -5,9 +5,7 @@ AWS.config.update({region: 'us-east-1'});
 
 let params = {
   Destination: { /* required */
-    ToAddresses: [
-      '',
-    ]
+    ToAddresses: []
   },
   Message: { /* required */
     Body: { /* required */
@@ -31,11 +29,11 @@ let params = {
   ],
 };
 
-function ses_sendemail(emailAddr, htmlEmail){
-    params.Destination.ToAddresses[0] = emailAddr;
+function ses_sendemail(emailAddrs, htmlEmail){
+    params.Destination.ToAddresses = emailAddrs;
     params.Message.Body.Html.Data = htmlEmail;
     params.Message.Body.Text.Data = htmlEmail; //TODO: plain text
-
+   
     // Create the promise and SES service object
     let sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
     
