644b73f8fb1db44c412b8e60bfd032d0d3c7d10a
Jon Wilson <jtwilson@ashevillenc.gov>
2019-05-29 15:16:25 -0400

    switched to service account credentials for Google

--- index.js
+++ index.js
@@ -1,123 +1,74 @@
-const fs = require('fs');
-const readline = require('readline');
 const {google} = require('googleapis');
+let privatekey = require('./client_secret.json');
 let checkForMatches = require('./checkForMatches');
-
 require('dotenv').config();
 
-// Adapted from https://developers.google.com/sheets/api/quickstart/nodejs
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile(__dirname + '/credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), getData);
+// configure a JWT auth client
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets.readonly']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+  if (err) {
+    console.log(err);
+  return;
+  }
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
+try {
+  getDivestmentData(jwtClient).then(divestment_vendors=>{
+    getApproved(jwtClient).then(approved_vendors=>{
+      checkForMatches(approved_vendors,divestment_vendors);
     });
   });
-}
-
-function getData(auth) {
-  try {
-      getDivestmentData(auth).then(divestment_vendors=>{
-        getApproved(auth).then(approved_vendors=>{
-          checkForMatches(approved_vendors,divestment_vendors);
-        });
-      });
-  } catch (err) {
-    console.error(err);
-  }
+} catch (err) {
+console.error(err);
 }
 
 function getDivestmentData(auth) {
   return new Promise(function(resolve, reject) {
-    const sheets = google.sheets({version: 'v4', auth});
+    let spreadsheetId = process.env.spreadsheetid;
+    let range = 'Bad Actors!A5:B';
+    let sheets = google.sheets('v4');
     sheets.spreadsheets.values.get({
-        spreadsheetId: process.env.spreadsheetid,
-        range: 'Bad Actors!A5:B',
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const divestment_vendors = res.data.values;
-        if (divestment_vendors.length) {
-          resolve(divestment_vendors);
-        } else {
-            console.log('No data found.');
-        }
+      auth,
+      spreadsheetId,
+      range
+    }, function (err, response) {
+      if (err) {
+        reject(new Error('The API returned an error: ' + err));
+      }         
+      const divestment_vendors = response.data.values;
+      if (divestment_vendors) {
+        resolve(divestment_vendors);
+      } else {
+        reject(new Error('No data found.'));
+      }
     });
   });
 }
 
 function getApproved(auth) {
   return new Promise(function(resolve, reject) {
-    const sheets = google.sheets({version: 'v4', auth});
+    let spreadsheetId = process.env.spreadsheetid;
+    let range = 'Approved Companies!A6:B';
+    let sheets = google.sheets('v4');
     sheets.spreadsheets.values.get({
-        spreadsheetId: process.env.spreadsheetid,
-        range: 'Approved Companies!A6:B',
-    }, (err, res) => { if(!res.data.values) { resolve(); return; }
-        if (err) reject('The API returned an error: ' + err);
-        const approved_vendors = res.data.values;
-        if (approved_vendors.length) {
-          resolve(approved_vendors);
-        } else {
-          reject('No data found.');
-        }
+      auth,
+      spreadsheetId,
+      range
+    }, function (err, response) {
+      if (err) {
+          reject(new Error('The API returned an error: ' + err));
+      }         
+      const approved_vendors = response.data.values;
+      if (approved_vendors) {
+        resolve(approved_vendors);
+      } else {
+        resolve(null);
+      }
     });
   });
 }
-
-process.on('uncaughtException', (err)=>{
-  console.log(err);
-});
