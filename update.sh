c24fe1efba82739842aefe11a07f24194fb9d923
Jon <jtwilson@ashevillenc.gov>
2020-06-26 14:10:49 -0400

    trying to update packages-not working yet

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 rm deploy.zip 2> /dev/null
 
-7z a -r -tzip deploy.zip *.js email.pug client_secret.json node_modules/*
+
+#For Windows, use 7z
+#7z a -r -tzip deploy.zip *.js email.pug client_secret.json node_modules/*
+zip -r deploy.zip *.js email.pug client_secret.json node_modules/*
 
 aws lambda update-function-code --function-name iran-divestment --zip-file fileb://deploy.zip
 
