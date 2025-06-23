c843232d3146bd71d2f4888ca0ddab0f79fee4d9
alletizia <aletizia529@gmail.com>
2025-06-23 11:09:26 -0400

    update Promise syntax due to errors

--- src/ses_sendemail.js
+++ src/ses_sendemail.js
@@ -1,50 +1,46 @@
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 const client = new SESClient({ region: 'us-east-1' });
 
-function ses_sendemail (emailAddrs, htmlEmail) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const params = {
-        Destination: {
-          /* required */
-          CcAddresses: [
-            /* more items */
-          ],
-          ToAddresses: emailAddrs
+async function ses_sendemail (emailAddrs, htmlEmail) {
+  const params = {
+    Destination: {
+      /* required */
+      CcAddresses: [
+        /* more items */
+      ],
+      ToAddresses: emailAddrs
+    },
+    Message: {
+      /* required */
+      Body: {
+        /* required */
+        Html: {
+          Charset: 'UTF-8',
+          Data: htmlEmail
         },
-        Message: {
-          /* required */
-          Body: {
-            /* required */
-            Html: {
-              Charset: 'UTF-8',
-              Data: htmlEmail
-            },
-            Text: {
-              Charset: 'UTF-8',
-              Data: htmlEmail
-            }
-          },
-          Subject: {
-            Charset: 'UTF-8',
-            Data: 'City of Asheville Iran Divestment'
-          }
-        },
-        Source: 'asheville_notifications@ashevillenc.gov', // SENDER_ADDRESS
-        ReplyToAddresses: [
-          'asheville_notifications@ashevillenc.gov'
-        ]
-      }
-
-      client.send(new SendEmailCommand(params), (err) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve('Success - Message ID:' + MessageID)
+        Text: {
+          Charset: 'UTF-8',
+          Data: htmlEmail
         }
-      })
-    } catch (err) { reject(err) }
-  })
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'City of Asheville Iran Divestment'
+      }
+    },
+    Source: 'asheville_notifications@ashevillenc.gov', // SENDER_ADDRESS
+    ReplyToAddresses: [
+      'asheville_notifications@ashevillenc.gov'
+    ]
+  }
+  try {
+    const data = await client.send(new SendEmailCommand(params));
+    console.log('Email sent successfully. Message ID:', data.MessageId);
+    return 'Success - Message ID:' + data.MessageId;
+  } catch (err) { 
+    console.error('Error sending email:', err);
+    throw err;
+  }
 };
 
 export default ses_sendemail;
